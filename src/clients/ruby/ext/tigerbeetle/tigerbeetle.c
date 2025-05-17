e77fc992ad7a071fb671229f46d476a6b8adcf6b
Trevor John <trevorrjohn@gmail.com>
2025-05-17 01:12:38 -0400

    WIP - client is setup, when I pass bad data it fails silently, not sure full issue yet
    - when I pass the commented out account, it does get to callback but
      then segfault

--- src/clients/ruby/ext/tigerbeetle/tigerbeetle.c
+++ src/clients/ruby/ext/tigerbeetle/tigerbeetle.c
@@ -5,7 +5,6 @@ tb_uint128_t rb_to_uint128(VALUE rb);
 
 typedef struct {
   tb_client_t client;
-  VALUE callback;
 } rb_tb_client_wrapper;
 
 static void rb_tb_client_mark(void *p);
@@ -24,38 +23,36 @@ static const rb_data_type_t rb_tb_client_type = {
   .flags = RUBY_TYPED_FREE_IMMEDIATELY,
 };
 
-void rb_tb_completion_callback(uintptr_t ctx, tb_packet_t* packet, uint64_t timestamp, const uint8_t* result_ptr, uint32_t result_len);
+typedef struct {
+  VALUE proc;
+  VALUE self;
+} rb_tb_proc_wrapper;
+
+static void rb_tb_proc_mark(void *p);
+static void rb_tb_proc_free(void *p);
+static size_t rb_tb_proc_memsize(const void *p);
+static const rb_data_type_t rb_tb_proc_type = {
+  .wrap_struct_name = "TigerBeetle::Ext::ProcWrapper",
+  .function = {
+    .dmark = rb_tb_proc_mark,
+    .dfree = rb_tb_proc_free,
+    .dsize = rb_tb_proc_memsize,
+  },
+  .data = NULL,
+  .flags = RUBY_TYPED_FREE_IMMEDIATELY,
+};
+
+void rb_tb_completion_callback(uintptr_t _ctx, tb_packet_t* packet, uint64_t timestamp, const uint8_t* result_ptr, uint32_t result_len);
 void handle_init_error_status(TB_INIT_STATUS status);
 static VALUE rb_tb_client_initialize(int argc, VALUE *argv, VALUE self);
-
-// typedef struct {
-//   tb_account_t account;
-//   VALUE rb_data;
-// } rb_tb_account_wrapper;
-//
-// static const rb_data_type_t rb_tb_account_type = {
-// }
-
-/*
-  * client = TigerBeetle::Client.new ...
-  * flags = TigerBeetle::Bindings::ACCOUNT_FLAGS::ACCOUNT_DEBITS_MUST_NOT_EXCEED_CREDITS
-  * data = [TigerBeetle::Bindings::Account.new(id: "123", user_data: "456", ledger: 10, code: 1, flags:)]
-  * operation = TigerBeetle::Bindings::TB_OPERATION::CREATE_ACCOUNT
-  * client.submit(data, operation)
-  *
-  * callback(ctx, packet, timestamp, result_ptr, result_len)
-  *
-  *
-  */
-// static VALUE rb_tb_client_submit(VALUE self, VALUE rb_data, VALUE rb_operation);
-// static VALUE rb_tb_client_submit(VALUE self, VALUE rb_data, VALUE rb_operation) {
-// }
+static VALUE rb_tb_client_submit(int argc, VALUE *argv, VALUE self);
 
 void Init_tigerbeetle() {
   VALUE mTigerBeetle = rb_define_module("TigerBeetle");
   VALUE rb_cClient = rb_define_class_under(mTigerBeetle, "Client", rb_cObject);
   rb_define_alloc_func(rb_cClient, rb_tb_client_alloc);
   rb_define_method(rb_cClient, "initialize", rb_tb_client_initialize, -1);
+  rb_define_method(rb_cClient, "submit", rb_tb_client_submit, -1);
 }
 
 tb_uint128_t rbignum_to_uint128(VALUE bignum);
@@ -153,23 +150,14 @@ tb_uint128_t rstring_to_uint128(VALUE rb_str) {
 }
 
 static void rb_tb_client_mark(void *p) {
-  rb_tb_client_wrapper *wrapper = (rb_tb_client_wrapper *)p;
-  if (wrapper && !NIL_P(wrapper->callback)) {
-    rb_gc_mark(wrapper->callback);
-  }
+  // noop
 }
 
 static void rb_tb_client_free(void *p) {
-  if (p) {
-    rb_tb_client_wrapper *wrapper = (rb_tb_client_wrapper *)p;
+  if (p == NULL) return;
 
-    // NOTE: I think this should be handled elsewhere not on GC
-    // if (wrapper->client) {
-    //   tb_client_deinit(&wrapper->client);
-    // }
-
-    xfree(wrapper);
-  }
+  rb_tb_client_wrapper *wrapper = (rb_tb_client_wrapper*)p;
+  xfree(wrapper);
 }
 
 static size_t rb_tb_client_memsize(const void *p) {
@@ -218,18 +206,13 @@ void handle_init_error_status(TB_INIT_STATUS status) {
 }
 
 static VALUE rb_tb_client_initialize(int argc, VALUE *argv, VALUE self) {
-  VALUE rb_addresses, rb_cluster_id, rb_callback, rb_echo;
-  rb_scan_args(argc, argv, "31", &rb_addresses, &rb_cluster_id, &rb_callback, &rb_echo);
+  VALUE rb_addresses, rb_cluster_id, rb_echo;
+  rb_scan_args(argc, argv, "21", &rb_addresses, &rb_cluster_id, &rb_echo);
 
   Check_Type(rb_addresses, T_STRING);
 
   rb_tb_client_wrapper *wrapper;
   TypedData_Get_Struct(self, rb_tb_client_wrapper, &rb_tb_client_type, wrapper);
-
-  if (!NIL_P(rb_callback) && !rb_respond_to(rb_callback, rb_intern("call"))) {
-    rb_raise(rb_eTypeError, "callback must respond to 'call'");
-  }
-  wrapper->callback = rb_callback;
 
   char *addresses = StringValueCStr(rb_addresses);
   uint128_t cluster_id_value = rb_to_uint128(rb_cluster_id);
@@ -255,7 +238,7 @@ static VALUE rb_tb_client_initialize(int argc, VALUE *argv, VALUE self) {
     cluster_id,
     addresses,
     address_len,
-    (uintptr_t)self,
+    0, //completion ctx
     rb_tb_completion_callback
   );
   handle_init_error_status(status);
@@ -263,17 +246,117 @@ static VALUE rb_tb_client_initialize(int argc, VALUE *argv, VALUE self) {
   return self;
 }
 
-void rb_tb_completion_callback(uintptr_t ctx, tb_packet_t* packet, uint64_t timestamp, const uint8_t* result_ptr, uint32_t result_len) {
-  rb_tb_client_wrapper *wrapper;
-  TypedData_Get_Struct((VALUE)ctx, rb_tb_client_wrapper, &rb_tb_client_type, wrapper);
-
-  if (NIL_P(wrapper->callback)) {
-    rb_raise(rb_eRuntimeError, "No callback set");
+void rb_tb_completion_callback(uintptr_t _ctx, tb_packet_t* packet, uint64_t timestamp, const uint8_t* result_ptr, uint32_t result_len) {
+  printf("rb_tb_completion_callback triggered");
+  if (packet == NULL) {
+    rb_raise(rb_eRuntimeError, "Packet is NULL");
+    return;
   }
 
-  /*
-    * client.completion_callback(packet, timestamp, result_str)
-    *
-    */
-  rb_funcall(wrapper->callback, rb_intern("call"), 1, (VALUE)ctx);
+  rb_tb_proc_wrapper* wrapper = (rb_tb_proc_wrapper*)packet->user_data;
+  if (!wrapper || !wrapper->self) {
+    xfree(packet);
+    rb_raise(rb_eRuntimeError, "Packet user_data freed before callback");
+    return;
+  }
+
+  if (!rb_respond_to(wrapper->proc, rb_intern("call"))) {
+    rb_gc_unregister_address(&wrapper->self);
+    xfree(packet);
+    rb_raise(rb_eTypeError, "callback must respond to 'call'");
+    return;
+  }
+
+  // extract the result data
+  VALUE status_code = INT2NUM(packet->status);
+  VALUE result_at = ULL2NUM(timestamp);
+  VALUE data;
+  if (result_len > 0) {
+    data = rb_str_new((const char *)result_ptr, result_len);
+  } else {
+    data = Qnil;
+  }
+
+  // create the result object
+  VALUE mTB = rb_const_get(rb_cObject, rb_intern("TigerBeetle"));
+  VALUE rbResult = rb_const_get(mTB, rb_intern("Result"));
+  VALUE args[3] = { status_code, result_at, data };
+  VALUE result = rb_funcall(rbResult, rb_intern("new"), 3, args);
+
+  // call the passed to submit callback
+  rb_funcall(wrapper->proc, rb_intern("call"), 1, result);
+
+  // release wrapper and free packet
+  rb_gc_unregister_address(&wrapper->self);
+  xfree(packet->data);
+  xfree(packet);
+}
+
+static VALUE rb_tb_client_submit(int argc, VALUE *argv, VALUE self) {
+  VALUE rb_data, rb_operation;
+  rb_scan_args(argc, argv, "2", &rb_data, &rb_operation);
+
+  if (!rb_block_given_p()) {
+    rb_raise(rb_eArgError, "Block required");
+  }
+
+  // need to create a callback wrapper and register for GC
+  rb_tb_proc_wrapper *callback_wrapper;
+  VALUE wrapper_obj = TypedData_Make_Struct(rb_cObject, rb_tb_proc_wrapper,
+                                            &rb_tb_proc_type, callback_wrapper);
+  callback_wrapper->proc = rb_block_proc();
+  callback_wrapper->self = wrapper_obj;
+  rb_gc_register_address(&callback_wrapper->self);
+
+  // create the packet data to be sent
+  tb_packet_t *packet = (tb_packet_t *)ZALLOC(tb_packet_t);
+  // tb_account_t* act = (tb_account_t*)ZALLOC(tb_account_t);
+  // act->id = 1;
+  // act->ledger = 1;
+  // act->code = 1;
+  // packet->data = act;
+  // packet->data_size = sizeof(tb_account_t);
+  packet->user_data = (void*)callback_wrapper;
+  // strdup to allocate the memory
+  char* str_data = StringValueCStr(rb_data);
+  size_t data_len = strlen(str_data) + 1;
+  char* data = ALLOC_N(char, data_len);
+  memcpy(str_data, data, data_len);
+  packet->data = data;
+  packet->data_size = (uint32_t)data_len;
+  packet->user_tag = 0;
+  packet->operation = (uint8_t)NUM2UINT(rb_operation);
+
+  // send the packet to the client
+  rb_tb_client_wrapper* client_wrapper;
+  TypedData_Get_Struct(self, rb_tb_client_wrapper, &rb_tb_client_type, client_wrapper);
+  TB_CLIENT_STATUS status = tb_client_submit(&client_wrapper->client, packet);
+
+  if (status == TB_CLIENT_OK) return Qtrue;
+
+  // there was an issue and the packet wasn't sent, free any memory allocated
+  rb_gc_unregister_address(&callback_wrapper->self);
+  free(packet->data);
+  xfree(packet);
+  return Qfalse;;
+}
+
+static void rb_tb_proc_mark(void *p) {
+  if (p == NULL) return;
+
+  rb_tb_proc_wrapper *wrapper = (rb_tb_proc_wrapper*)p;
+  if (wrapper->proc != Qnil) {
+    rb_gc_mark(wrapper->proc);
+  }
+}
+
+static void rb_tb_proc_free(void *p) {
+  if (p == NULL) return;
+
+  rb_tb_proc_wrapper *wrapper = (rb_tb_proc_wrapper *)p;
+  xfree(wrapper);
+}
+
+static size_t rb_tb_proc_memsize(const void *p) {
+  return sizeof(rb_tb_proc_wrapper);
 }
---
+++ src/clients/ruby/lib/tigerbeetle/result.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TigerBeetle
+  Result = Data.define(:status_code, :result_at, :data)
+end
