308303e559c3e8c6072823694393185eb1cb6652
Trevor John <trevorrjohn@gmail.com>
2025-05-22 01:55:43 -0400

    Updating client to support submitting packets

--- src/clients/ruby/ext/tigerbeetle/tigerbeetle.c
+++ src/clients/ruby/ext/tigerbeetle/tigerbeetle.c
@@ -10,6 +10,7 @@ tb_uint128_t rb_to_uint128(VALUE rb);
 void completion_callback(uintptr_t ctx, tb_packet_t* packet, uint64_t timestamp, const uint8_t* data, uint32_t data_size);
 
 VALUE rb_tb_client_init(int argc, VALUE *argv, VALUE self);
+VALUE rb_tb_client_submit(int argc, VALUE *argv, VALUE self);
 
 void Init_tigerbeetle(void) {
   VALUE tb_module = rb_define_module("TigerBeetle");
@@ -20,6 +21,7 @@ void Init_tigerbeetle(void) {
 
   VALUE tb_client_class = rb_const_get(bindings_module, rb_intern("Client"));
   rb_define_method(tb_client_class, "init", rb_tb_client_init, -1);
+  rb_define_method(tb_client_class, "submit", rb_tb_client_submit, -1);
 }
 
 VALUE rb_tb_client_init(int argc, VALUE *argv, VALUE self) {
@@ -76,4 +78,34 @@ VALUE rb_tb_client_init(int argc, VALUE *argv, VALUE self) {
 }
 
 void completion_callback(uintptr_t ctx, tb_packet_t* packet, uint64_t timestamp, const uint8_t* data, uint32_t data_size) {
+  printf("Got the packet");
+}
+
+VALUE rb_tb_client_submit(int argc, VALUE *argv, VALUE self) {
+  VALUE kwargs;
+  rb_scan_args(argc, argv, ":", &kwargs);
+
+  tb_client_t *client;
+  TypedData_Get_Struct(self, tb_client_t, &rb_tb_client_type, client);
+
+  tb_packet_t *packet;
+  VALUE rb_packet = rb_hash_aref(kwargs, ID2SYM(rb_intern("packet")));
+  TypedData_Get_Struct(rb_packet, tb_packet_t, &rb_tb_packet_type, packet);
+  if (packet == NULL) {
+    rb_raise(rb_eArgError, "rb_data must not be nil");
+  }
+  TB_CLIENT_STATUS status = tb_client_submit(client, packet);
+
+  switch(status) {
+    case TB_CLIENT_OK:
+      return Qtrue;
+      break;
+    case TB_CLIENT_INVALID:
+      rb_raise(rb_eRuntimeError, "Unexpected error during initialization");
+      break;
+    default:
+      rb_raise(rb_eRuntimeError, "Unknown error during initialization");
+      break;
+  }
+  return Qfalse;
 }
