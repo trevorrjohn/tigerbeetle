c5d3470470dd427c6d48bdf49c323f83fd82bfd0
Trevor John <trevorrjohn@gmail.com>
2025-05-22 14:02:59 -0400

    Getting the completion callback, now need to trigger the block

--- src/clients/ruby/samples/main.rb
+++ src/clients/ruby/samples/main.rb
@@ -7,7 +7,7 @@ require "tigerbeetle"
 PORT = ENV.fetch("TB_PORT", "3000").to_s
 CLUSTER_ID = ENV.fetch("TB_CLUSTER_ID", "0").to_i
 ledger = ENV.fetch("TB_LEDGER", "1").to_i
-code = ENV.fetch("TB_CODE", "0").to_i
+code = ENV.fetch("TB_CODE", "1").to_i
 
 client = TigerBeetle.connect(addresses: PORT, cluster_id: CLUSTER_ID)
 
@@ -21,9 +21,13 @@ account2 = TigerBeetle::Bindings::Account.new(
   id: "1", ledger:, code:, flags: TigerBeetle::Bindings::AccountFlags::CREDITS_MUST_NOT_EXCEED_DEBITS
 )
 
-results_queue = Queue.new
+results = client.lookup_accounts([1, 2]) do |res|
+  puts res
+end
+puts results_queue.pop
 
 client.create_accounts(account1, account2) do |results|
+  puts "here"
   results_queue << results
 end
 
